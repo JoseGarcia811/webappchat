e12bcabde63ba0970966d28654e0abff0bf858cd
Jose Garcia Vargas <j.garcia05@live.com>
2024-05-06 16:49:05 -0400

    mmmm

---
+++ src/app/services/chat.service.ts
@@ -0,0 +1,46 @@
+ 
+import { ChatMessage } from '../models/chat-message';
+import { BehaviorSubject } from 'rxjs';
+import SockJS from 'sockjs-client';
+import { Stomp } from '@stomp/stompjs';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  private stompClient: any
+  private messageSubject: BehaviorSubject<ChatMessage[]> = new BehaviorSubject<ChatMessage[]>([]);
+
+  constructor() { 
+    this.initConnenctionSocket();
+  }
+
+  initConnenctionSocket() {
+    const url = 'https://chatwebsocket-idvc.onrender.com/chat-socket';
+    const socket = new SockJS(url);
+    this.stompClient = Stomp.over(socket);
+  }
+
+  joinRoom(roomId: string) {
+    this.stompClient.connect({}, ()=>{
+      this.stompClient.subscribe(`/topic/${roomId}`, (messages: any) => {
+        const messageContent = JSON.parse(messages.body);
+        const currentMessage = this.messageSubject.getValue();
+        currentMessage.push(messageContent);
+
+        this.messageSubject.next(currentMessage);
+
+      })
+    })
+  }
+
+  sendMessage(roomId: string, chatMessage: ChatMessage) {
+    this.stompClient.send(`/app/chat/${roomId}`, {}, JSON.stringify(chatMessage))
+  }
+
+  getMessageSubject(){
+    return this.messageSubject.asObservable();
+  }
+}
